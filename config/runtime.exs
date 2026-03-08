0e9bc8f37f9016c0b4db86683540602cb9037d4a
Daniel Benjamin <benjamindaniel706@gmail.com>
2026-03-08 07:18:26 +0100

    fix(backend): allow chrome-extension WebSocket origins in production

--- config/runtime.exs
+++ config/runtime.exs
@@ -59,6 +59,7 @@ if config_env() == :prod do
 
   config :open_party, OpenPartyWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["https://" <> host, ~r/^chrome-extension:\/\//],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
